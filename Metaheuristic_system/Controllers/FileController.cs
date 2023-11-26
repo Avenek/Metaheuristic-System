95e1cb68afc979cb2ac39004b3fc80353b694dd7
Avenek <j.machnik2002@gmail.com>
2023-11-26 21:29:30 +0100

    Uploading files

--- Metaheuristic_system/Controllers/FileController.cs
+++ Metaheuristic_system/Controllers/FileController.cs
@@ -2,6 +2,7 @@
 using Metaheuristic_system.Models;
 using Metaheuristic_system.Services;
 using Microsoft.AspNetCore.Mvc;
+using System.IO;
 
 namespace Metaheuristic_system.Controllers
 {
@@ -12,9 +13,15 @@ namespace Metaheuristic_system.Controllers
         [HttpPost]
         public ActionResult Upload([FromForm] IFormFile file)
         {
-            if(file == null && file.Length > 0)
+            if(file != null && file.Length > 0)
             {
                 string path = "/dll";
+
+                if (!Directory.Exists(path))
+                {
+                    Directory.CreateDirectory(path);
+                }
+
                 string fileName = file.FileName;
                 string fullPath = $"{path}/{fileName}";
                 using(var stream = new FileStream(fullPath, FileMode.Create))
